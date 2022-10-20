e852b69a2d1b42c3f41d861f363760c26e349312
jcamilorg <juancamilorg64@gmail.com>
2022-10-20 07:32:57 -0500

    html dinamico

--- main.ts
+++ main.ts
@@ -8,4 +8,17 @@ export const ap = new Aprendiz(
   NivelEducativo.UNIVERSITARIO
 );
 
-console.log(ap);
+let $aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+
+mostrarDatosAprendiz(ap);
+
+function mostrarDatosAprendiz(aprendiz: Aprendiz): void {
+  let tbody = document.createElement("tbody");
+  tbody.innerHTML = `
+  <tr><td colspan=2><img src=${aprendiz.avatars} height="100"></td></tr>
+  <tr><td>Nombres:</td><td>${aprendiz.nombres}</td></tr>
+  <tr><td>Apellidos:</td><td>${aprendiz.apellidos}</td></tr>
+  <tr><td>Nivel Educactivo:</td><td>${aprendiz.nivelEducativo}</td></tr>
+  <tr><td>Edad:</td><td>${aprendiz.edad}</td></tr>`;
+  $aprendizTable.appendChild(tbody);
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 export var ap = new Aprendiz("Juan Camilo", "Ramirez Gonzalez", "https://i.pinimg.com/736x/9f/85/8e/9f858e7ae6ec0895dab7b6053d05b3ad.jpg", 22, NivelEducativo.UNIVERSITARIO);
-console.log(ap);
+var $aprendizTable = document.getElementById("aprendiz");
+mostrarDatosAprendiz(ap);
+function mostrarDatosAprendiz(aprendiz) {
+    var tbody = document.createElement("tbody");
+    tbody.innerHTML = "\n  <tr><td colspan=2><img src=".concat(aprendiz.avatars, " height=\"100\"></td></tr>\n  <tr><td>Nombres:</td><td>").concat(aprendiz.nombres, "</td></tr>\n  <tr><td>Apellidos:</td><td>").concat(aprendiz.apellidos, "</td></tr>\n  <tr><td>Nivel Educactivo:</td><td>").concat(aprendiz.nivelEducativo, "</td></tr>\n  <tr><td>Edad:</td><td>").concat(aprendiz.edad, "</td></tr>");
+    $aprendizTable.appendChild(tbody);
+}
